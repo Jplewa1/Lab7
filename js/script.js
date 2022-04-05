70a86a8fb05d7ca95c4ebc4ddbf55a290efd75bd
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-05 13:09:23 -0500

    finish and test

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ function init(){
       alert("Jordan Plewa: " + j1);
       j2.innerHTML = j1;
   }
-
+  var j3 = document.getElementById("entrybutton");
+  j3.addEventListener("click" , alertFunction);
+}
+window.addEventListener('load' , init);
     
     
     
