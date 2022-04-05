a23bfb4e8b29f6f2fd15c2f431b8a2d9b0447b1b
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-05 13:13:24 -0500

    fix cascade

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
     //add your javascrip between these two lines of code
   function alertFunction(){
-      var j1 = document.getElementById("entryinput").value;
-      var j2 = document.getElementById("textoutput");
-      alert("Jordan Plewa: " + j1);
-      j2.innerHTML = j1;
+    var j1 = document.getElementById("entryinput").value;
+    var j2 = document.getElementById("textoutput");
+    alert("Jordan Plewa: " + j1);
+    j2.innerHTML = j1;
   }
   var j3 = document.getElementById("entrybutton");
   j3.addEventListener("click" , alertFunction);
